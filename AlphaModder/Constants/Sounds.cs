cc273fb7cacc8286ce8eea0a728384a2faa5100b
pat-f <pat-f@users.noreply.github.com>
2020-05-30 02:15:19 -0400

    adding sound constants

--- AlphaModder/Constants/Sounds.cs
+++ AlphaModder/Constants/Sounds.cs
@@ -8,6 +8,65 @@ namespace AlphaModder.Constants
 {
     class Sounds
     {
+        // fx folder
+        public const String ARTIFACT_LOST = "//fx//cpu artifact lost.wav";
+        public const String DATA_POD_RECOVERED = "//fx//cpu data pod recovered.wav";
+        public const String DRONE_RIOTS_HAVE_ENDED = "//fx//cpu drone riots over.wav";
+        public const String DRONE_RIOTS = "//fx//cpu drone riots.wav";
         public const String ENEMY_PROJECT_INITIATED = "//fx//CPU enemy project initiated.wav";
+        public const String ENERGY_RESOURCES = "//fx//CPU energy resources.wav";
+        public const String FACTION_ERADICATED = "//fx//cpu faction gone.wav";
+        public const String GOLDEN_AGE_BEGUN = "//fx//cpu golden age begun.wav";
+        public const String GOLDEN_AGE_ENDED = "//fx//cpu golden age over.wav";
+        public const String IMPROVEMENT_ALREADY_EXISTS = "//fx//cpu improved already.wav";
+        public const String IMPROVEMENT_DESTROYED = "//fx//cpu improvement destroyed.wav";
+        public const String INDIGENOUS_LIFEFORMS = "//fx//CPU indigenous lifeforms.wav";
+        public const String INSUFFICIENT_ENERGY = "//fx//cpu insufficient energy.wav";
+        public const String INVALID_BASE_SITE = "//fx//cpu invalid base site.wav";
+        public const String INVALID_TERRAFORM_COMMAND = "//fx//cpu invalid terraform.wav";
+        public const String UNIT_LACKS_REQUIRED_ABILITY = "//fx//cpu lacks ability.wav";
+        public const String MAXIMUM_POPULATION_REACHED = "//fx//cpu max population.wav";
+        public const String MISSION_ACCOMPLISHED = "//fx//cpu mission accomplished.wav";
+        public const String MISSION_ABORTED = "//fx//cpu mission aborted.wav";
+        public const String NEW_ORDERS_NEEDED = "//fx//cpu new orders.wav";
+        public const String NETWORK_NODE_ALREADY_LINKED = "//fx//cpu nn already linked.wav";
+        public const String CANNOT_EXECUTE_ORDER = "//fx//cpu nocando.wav";
+        public const String NUTRIENT_RESOURCES = "//fx//CPU nutrient resources.wav";
+        public const String PLEASE_DONT_GO = "//fx//CPU please don't go.wav";
+        public const String POD_RECOVERED = "//fx//cpu pod recovered.wav";
+        public const String PRODUCTION_COMPLETE = "//fx//cpu prod complete.wav";
+        public const String PROJECT_INITIATED = "//fx//CPU project initiated.wav";
+        public const String PROBE_TEAM_COMPROMISED = "//fx//cpu pt compromised.wav";
+        public const String REQUEST_CONFIRMATION = "//fx//cpu request confirmation.wav";
+        public const String RESOURCE_SHORTFALL = "//fx//cpu resource shortfall.wav";
+        public const String PERHAPS_CAREFUL_REVIEW = "//fx//CPU review your options.wav";
+        public const String SYSTEM_ERROR = "//fx//cpu system error.wav";
+        public const String TERRAFORM_COMPLETE = "//fx//CPU terraform complete.wav";
+        public const String TURN_COMPLETE = "//fx//cpu turn complete.wav";
+        public const String UNIT_LOST = "//fx//CPU unit lost.wav";
+        public const String ENERGY_WEAPON_1 = "//fx//energy1.wav";
+        public const String JET = "//fx//jet.wav";
+        public const String MINDWORMS = "//fx//mindworms.wav";
+        public const String OK = "//fx//ok.wav";
+        public const String MAIN_MENU_MUSIC = "//fx//opening menu.wav";
+        public const String PLANET_BUSTER_1 = "//fx//planet buster lead.wav";
+        public const String PLANET_BUSTER_2 = "//fx//planet buster.wav";
+        public const String PSI_ATTACK = "//fx//psi attack.wav";
+        public const String MINDWORMS_DESTROYED = "//fx//psi blow.wav";
+
+        // TODO ORGANIZE MENU SOUNDS
+        public const String COMMLINK_MENU = "//voices//mmenu.wav";
+
+
+        // voices folder - faction openings
+        public const String BELIEVERS_OPENING = "//voices//believe.wav";
+        public const String GAIANS_OPENING = "//voices//gaians.wav";
+        public const String HIVE_OPENING = "//voices//hive.wav";
+        public const String MORGANITES_OPENING = "//voices//morgan.wav";
+        public const String PEACEKEEPERS_OPENING = "//voices//peace.wav";
+        public const String SPARTANS_OPENING = "//voices//spartans.wav";
+        public const String UNIVERSITY_OPENING = "//voices//univ.wav";
+
+
     }
 }
