591cd13d56db5f02ce051ebbb166f60cc99a2a23
pat-f <pat-f@users.noreply.github.com>
2020-05-31 14:36:53 -0400

    refactoring messages

---
+++ AlphaModder/Utils/DialogUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphaModder.Utils
+{
+    class DialogUtils
+    {
+        public static void messageBox(String message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public static bool messageBoxOkCancel(String message)
+        {
+            DialogResult dialogResult = MessageBox.Show(message, "", MessageBoxButtons.OKCancel);
+            if (dialogResult == DialogResult.OK)
+                return true;
+            return false;
+        }
+
+    }
+}
